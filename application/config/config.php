c2ef8be1ff1e7f3b5ad40a483bfe1af02a53bb75
ARBhuiyanSP <arbhuiyan.pits@gmail.com>
2019-04-30 10:11:48 +0600

    menu drop & logo add

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://saifbarcode.local/';
+$config['base_url'] = 'http://localhost/saif_barcode/';
 
 /*
 |--------------------------------------------------------------------------
